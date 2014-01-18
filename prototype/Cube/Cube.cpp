0de46d255c23005ad076fd9f65b2927c922d0b6b
srgg6701 <srgg6701@gmail.com>
2014-01-19 01:58:17 +0400

    Построили прекрасный белый квадрат. Начинаем препарировать.

--- prototype/Cube/Cube.cpp
+++ prototype/Cube/Cube.cpp
@@ -4,28 +4,44 @@
 #include <iostream>
 using namespace std;
 
-void Display()
+void init(void)
 {
+	//Выбрать фоновый (очищающий) цвет
+	glClearColor(0.4,0.0,0.0,0.0);
+	//Установить проекцию
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
+}
+void display(void)
+{
+	//Очистить экран 
+	glClear(GL_COLOR_BUFFER_BIT);
+	//Нарисовать белый полигон (квадрат) с углами //в (0.25, 0.25, 0.0) и (0.75, 0.75, 0.0)
+	glColor3f(1.0,1.0,1.0);
 	glBegin(GL_POLYGON);
 		glVertex3f(0.25,0.25,0.0);
 		glVertex3f(0.75,0.25,0.0);
 		glVertex3f(0.75,0.75,0.0);
 		glVertex3f(0.25,0.75,0.0);
 	glEnd();
-}
-void main(int argcv, char** argv)
-{
-	glutInit(&argcv, argv);
-	glutInitWindowSize(400,400);			// задать первоначальные размеры окна
-	glutInitWindowPosition(600,200); 	// задать отступы окна с графикой относительно 
-	glutCreateWindow("Window");
-
-	glClearColor(0.0,0.0,0.0,1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0,1.0,1.0);
-	glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
-	glutDisplayFunc(Display);
-	glutMainLoop();
+	//Не ждем. Начинаем выполнять буферизованные
+	//команды OpenGL
 	glFlush();
 }
-
+//Установить начальные характеристики окна,
+//открыть окно с заголовком «hello».
+//Зарегистрировать дисплейную функцию обратного вызова
+//Войти в главный цикл
+int main(int argc, char **argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowSize(250,250);
+	glutInitWindowPosition(100,100);
+	glutCreateWindow("Square");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
