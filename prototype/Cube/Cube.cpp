a925e0c09c3b676a14d039b32aa648a91d206ac3
srgg6701 <srgg6701@gmail.com>
2014-01-19 13:56:11 +0400

    Добавлен вывод лога, убраны ошибки.

--- prototype/Cube/Cube.cpp
+++ prototype/Cube/Cube.cpp
@@ -1,8 +1,20 @@
 #include "stdafx.h"
 #include "windows.h"
 #include <GL/glut.h>
+#include "vars.h"
 #include <iostream>
 using namespace std;
+
+void showLog()
+{
+	cout<<"gluLookAt:"<<endl
+		<<"eyeX (<:>) : "<<eyeX<<endl
+		<<"eyeY (,:.) : "<<eyeY<<endl
+		<<"eyeZ ([:]) : "<<eyeZ<<endl
+		<<"centerX (x:X) : "<<centerX<<endl
+		<<"centerY (y:Y) : "<<centerY<<endl
+		<<"centerZ (z:Z) : "<<centerZ<<endl;
+}
 
 //Инициализация
 void init(void)
@@ -18,13 +30,27 @@ void display(void)
 	//Очистить матрицу
 	glLoadIdentity();
 	//Видовая трансформация(камера)
-	gluLookAt(0.0,0.0,5.0,0.0,0.0,0.0,0.0,1.0,0.0);
+	gluLookAt(	// Задает видовую матрицу и умножает на нее текущую матрицу
+				// позиция наблюдателя; точка наблюдения
+				eyeX, // eye x
+				eyeY, // eye y
+				eyeZ, // eye z
+				// направление камеры (взгляда наблюдателя)
+				centerX, // x
+				centerY, // y 
+				centerZ, // z
+				// 
+				0.0,
+				1.0,
+				0.0
+			 );
 	//Модельная трансформация
 	glScalef( 1.5,
 			  1.5,
 			  1.5
 			);
 	glutWireCube(1.0);
+	showLog();
 	glFlush();
 }
 //Изменение размеров окна
@@ -36,6 +62,68 @@ void reshape(int w, int h)
 	glFrustum(-1.0,1.0,-1.0,1.0,1.5,20.0);
 	glMatrixMode(GL_MODELVIEW);
 }
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch(key) // см. справочник клавиатурных кодов ASCII здесь: http://www.theasciicode.com.ar/
+	{
+		case 60:	// <
+			eyeX-=stepXY;
+			break;
+		case 62:	// >
+			eyeX+=stepXY;
+			break;
+		case 44:	// ,
+			eyeY-=stepXY;
+			break;
+		case 46:	// .
+			eyeY+=stepXY;
+			break;
+		case 91:	// [
+			eyeZ-=stepXY;
+			break;
+		case 93:	// ]
+			eyeZ+=stepXY;
+			break;
+		//..................
+		case 120:	// x
+			centerX-=stepXY;
+			break;
+		case 88:	// X
+			centerX+=stepXY;
+			break;
+		case 121:	// y
+			centerY-=stepXY;
+			break;
+		case 89:	// Y
+			centerY+=stepXY;
+			break;
+		case 122:	// z
+			centerZ-=stepCZ;
+			break;
+		case 90:	// Z
+			centerZ+=stepCZ;
+			break;
+		/**/
+		case 61: // вернуться к первоначальным значениям
+			eyeX=EX;
+			eyeY=EY;
+			eyeZ=EZ;
+			centerX=CnX;
+			centerY=CnY;
+			centerZ=CnZ;
+			break;
+		// Закрыть окно по нажатию кл. "Пробел":
+		case 32: exit(0);
+			break;
+	}
+	if(key!=32) 
+	{
+		cout<<"pushed: "<<key<<endl;
+		glutPostRedisplay(); // перерисовать окно 
+	}
+}
+
+
 int main(int argc, char** argv)
 {
 	glutInit(&argc,argv);
@@ -46,6 +134,7 @@ int main(int argc, char** argv)
 	init();
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
+	glutKeyboardFunc(Keyboard);
 	glutMainLoop();
 	return 0;
 }
---
+++ prototype/Cube/vars.h
@@ -0,0 +1,16 @@
+const float EX = 0.0; 
+const float EY = 0.0; 
+const float EZ = 5.0; 
+float eyeX = EX;
+float eyeY = EY;
+float eyeZ = EZ;
+
+const float CnX = 0.0; 
+const float CnY = 0.0; 
+const float CnZ = 0.0; 
+float centerX = CnX;
+float centerY = CnY;
+float centerZ = CnZ;
+
+float stepXY = 0.5;
+float stepCZ = 2.0;
