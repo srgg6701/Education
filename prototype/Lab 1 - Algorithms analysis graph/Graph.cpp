6329350f71878f7cd061dfb1e038f068580959bd
srgg6701 <srgg6701@gmail.com>
2014-01-28 14:38:11 +0400

    Модифицирована процедура сортировки данных, извлечённых из текстового файла.

--- prototype/Lab 1 - Algorithms analysis graph/Graph.cpp
+++ prototype/Lab 1 - Algorithms analysis graph/Graph.cpp
@@ -249,3 +249,7 @@ int _tmain(int argc, char** argv)
 	glutMainLoop();
 	return 0;
 }
+/*	Материалы:
+	- Оценка эффективности алгорима по времени: http://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC#.D0.92.D1.80.D0.B5.D0.BC.D1.8F_.D1.80.D0.B0.D0.B1.D0.BE.D1.82.D1.8B
+	
+	*/
