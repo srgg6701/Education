91f707443adef515cb326920719cb3d8a3e713fb
srgg6701 <srgg6701@gmail.com>
2014-02-03 19:53:49 +0400

    Массовая препарация всевозможных видов/типов строк.

--- prototype/search_straight/search_straight.cpp
+++ prototype/search_straight/search_straight.cpp
@@ -6,24 +6,36 @@
 
 using namespace std;
 
-/*int straightFind()
+int straightFind()
 {
-    vector<int> rowsArray;
-	ifstream text("file_1000.txt");
-	int count = 0;
-	// включить кириллицу:
-	//setlocale(LC_ALL, "Russian");
-	//cout<<endl<<"До сортировки:"<<endl
-		//<<"................"<<endl;
-	while (text.good())
+    //vector<int> rowsArray;
+	ifstream text("file_10.txt");
+	
+	string strs = "2002";
+	// включить кириллицу: setlocale(LC_ALL, "Russian");
+	string int2str;
+	cout<<"strs length = "<<strs.length()<<endl;
+	for (unsigned i=0; i<strs.length(); ++i)
+		cout<<" :: "<<strs.at(i)<<endl;
+	/*while (text.good())
 	{
-		int v; text >> v;
-		rowsArray.push_back(v);
-		//cout<<v<<" ";
-	}	//cout<<endl;
+		int v; 
+		text >> v;
+		int2str = to_string(v);
+		cout<<"int2str: "<<int2str<<endl;
+		for (unsigned i=0; i<int2str.length(); ++i)
+		{
+			cout<<"Current character is: "<< int2str.at(i)<<endl;
+			if(int2str.at(i)==strs) 
+			{
+				cout<<endl<<" *found* character "<<int2str.at(i)<<endl;
+				break;
+			}
+		}
+	}*/	
 	
 
-	int i, j;
+	/*int i, j;
     int n = (int) rowsArray.size();
     int m = (int) find_str.size();
     i = -1 + _start;
@@ -38,14 +50,32 @@ using namespace std;
     {
         return i;
     }
-    else return -1;
-}*/
+    else return -1;*/
+	return 0;
+}/**/
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int nmb = 12345;
-	string str = to_string(nmb);
-	cout<<"My string now is "<<str<<endl;
+	int test = 2;
+	if(test==1)
+	{
+		int nmb = 12345;
+		string str = to_string(nmb);
+		char strs = '4';
+		cout<<"My string now is "<<str<<endl;
+		for (unsigned i=0; i<str.length(); ++i)
+		{
+			cout<<"Current character is: "<< str.at(i)<<endl;
+			if(str.at(i)==strs) 
+			{
+				cout<<endl<<" *found* character "<<str.at(i)<<endl;
+				break;
+			}
+		}
+	}
+	if(test==2)
+		straightFind();
+
 	return 0;
 }
 
